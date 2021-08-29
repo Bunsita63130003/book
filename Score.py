96b1eb49eaf7d02b4a4c5225bde50c2326a8091a
Bunsita63130003 <88535039+Bunsita63130003@users.noreply.github.com>
2021-08-30 01:00:43 +0700

    Add files via upload

---
+++ Score.py
@@ -0,0 +1,13 @@
+score = float(input("input your score: "))
+
+
+
+if(score<50): print("FAIL: E/F")
+elif(score<55 ): print("VERY POOR: D")
+elif(score<60): print("POOR: D+")
+elif(score<65): print("FAIR: C")
+elif(score<70): print("FAIRLY GOOD: C+")
+elif(score<75): print("GOOD: B")
+elif(score<80): print("VERY GOOG: B+")
+elif(score<=100): print("EXCELLENT: A")
+else:print("PUT SCORE AGAIN NA JA :)")
